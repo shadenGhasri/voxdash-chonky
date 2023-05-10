9b524591953a4caa39539e37ce35c5e4009812d2
shaden <92798768+shadenGhasri@users.noreply.github.com>
2023-05-10 15:57:11 +0330

    commit

--- src/components/file-list/ListContainer.tsx
+++ src/components/file-list/ListContainer.tsx
@@ -70,7 +70,7 @@ export const ListContainer: React.FC<FileListListProps> = React.memo(props => {
                     itemKey={getItemKey}
                     height={35}
                     itemCount={1}
-                    itemSize={500}
+                    itemSize={800}
                     layout="horizontal"
                     width={1000}
                    
