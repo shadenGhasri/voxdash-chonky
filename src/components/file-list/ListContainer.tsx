a2f1ca3ffe5859d3251638fa0f9ccbeb2faa090d
shaden <92798768+shadenGhasri@users.noreply.github.com>
2023-05-14 13:25:21 +0330

    commit

--- src/components/file-list/ListContainer.tsx
+++ src/components/file-list/ListContainer.tsx
@@ -136,8 +136,8 @@ const useStyles = makeLocalChonkyStyles(theme => ({
     titleDisplay: {
         display: 'flex',
         flexDirection: 'row',
-        width: '100%',
-        backgroundColor : "red"
+        width: '100vw',
+        // backgroundColor : "red"
     },
     titlecellDisplay: {
         fontSize: theme.listFileEntry.propertyFontSize,
